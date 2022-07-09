5f68e46e956933f62e19dee05cc04a0b9f56d9ca
lukehongg <79990740+lukehongg@users.noreply.github.com>
2022-07-09 20:48:38 +0900

    Update A030.java

--- A030.java
+++ A030.java
@@ -1,39 +1,27 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class A030 {
-	public static void run(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int N = input.nextInt();
-		int today = input.nextInt();
+	public static void run(String[] args) throws IOException {
 		
-		float[] percent = new float[2];
-		
-		for(int i=0; i<4; i++) {
-			percent[i] = input.nextFloat();
-			
-		}
-		float[][] happy = new float[2][N+1];
-		float[][] sad = new float[2][N+1];
-		
-		// happy -> happy
-		happy[0][0] = 1;
-		// sad -> happy
-		happy[1][0] = 1;
-		
-		// happy -> sad
-		sad[0][0] = 1;
-		// sad -> sad
-		sad[1][0] = 1;
-		
-		
-		
-		
-		
-		for(int i=1; i<=N; i++) {
-			
-		}
-		
-		
+	        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	        StringBuilder out = new StringBuilder();
+
+	        StringTokenizer st = new StringTokenizer(in.readLine());
+	        int N = Integer.parseInt(st.nextToken());
+	        double M = Integer.parseInt(st.nextToken());
+
+	        double[] arr = Arrays.stream(in.readLine().split(" ")).mapToDouble(Double::parseDouble).toArray();
+	        M = (1 - M) * 1000;
+
+	        for (int i = 0; i < N; ++i) M = M * arr[0] + (1000 - M) * arr[2];
+
+	        System.out.println(String.format("%.0f", M));
+	        System.out.println(String.format("%.0f", 1000 - M));
+	    
 	}
 
 }
